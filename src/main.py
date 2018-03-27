4b11a76052900adcd66f48fc6637391ba7c6617c
phpvicari <peehvicari@gmail.com>
2018-03-26 22:57:57 -0300

    ate onde deu, resolver como vai fazer o resto amanha

--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ from src.norma_machine import NormaMachine
 nm = NormaMachine()
 
 nm.set_n_to_reg("A",1)
+nm.is_primal("A")
 nm.set_n_to_reg("A",5)
+nm.is_primal("A")
 nm.set_n_to_reg("A",-1)
 nm.set_n_to_reg("A",-4)
 nm.set_n_to_reg("A",-2)
 nm.set_n_to_reg("A",4)
-nm.set_n_to_reg("A",2)
+nm.set_n_to_reg("A",9)
+nm.is_primal("A")
+nm.set_n_to_reg("A",11)
+nm.is_primal("A")
+
 
--- src/norma_machine.py
+++ src/norma_machine.py
@@ -1,3 +1,6 @@
+import math
+
+
 class NormaMachine:
 
     def __init__(self):
@@ -37,34 +40,72 @@ class NormaMachine:
                 self.registers[reg] -= 1
             print(self)
 
-    def add_b_to_a(self):
+    def add_b_to_a(self, regB, regA):
         print("Add B to A")
-        while self.registers["B"] != 0:
-            if self.registers["B"] > 0:
-                self.registers["A"] += 1
-                self.registers["B"] -= 1
+        while self.registers[regB] != 0:
+            if self.registers[regB] > 0:
+                self.registers[regA] += 1
+                self.registers[regB] -= 1
             else:
-                self.registers["A"] -= 1
-                self.registers["B"] += 1
+                self.registers[regA] -= 1
+                self.registers[regB] += 1
             print(self)
 
-    def add_b_to_a_with_c(self):
+    def add_b_to_a_with_c(self, regB, regA, regC):
         print("Add B to A with C")
-        while self.registers["B"] != 0:
-            if self.registers["B"] > 0:
-                self.registers["A"] += 1
-                self.registers["B"] -= 1
-                self.registers["C"] += 1
+        while self.registers[regB] != 0:
+            if self.registers[regB] > 0:
+                self.registers[regA] += 1
+                self.registers[regB] -= 1
+                self.registers[regC] += 1
             else:
-                self.registers["A"] -= 1
-                self.registers["B"] += 1
-                self.registers["C"] -= 1
+                self.registers[regA] -= 1
+                self.registers[regB] += 1
+                self.registers[regC] -= 1
             print(self)
-        while self.registers["C"] != 0:
-            if self.registers["C"] > 0:
-                self.registers["B"] += 1
-                self.registers["C"] -= 1
+        while self.registers[regC] != 0:
+            if self.registers[regC] > 0:
+                self.registers[regB] += 1
+                self.registers[regC] -= 1
             else:
-                self.registers["B"] -= 1
-                self.registers["C"] += 1
+                self.registers[regB] -= 1
+                self.registers[regC] += 1
             print(self)
+
+    def set_b_to_a_with_c(self):
+        self.set_0_to_reg("A")
+        self.add_b_to_a_with_c("B", "A", "C")
+
+    def mult_a_with_b(self):
+        while self.registers["A"] != 0:
+            self.registers["C"] += 1
+            self.registers["A"] -= 1
+            while self.registers["C"] != 0:
+                self.add_b_to_a_with_c("B", "A", "D")
+                self.registers["C"] -= 1
+
+    def test_a_lower_than_b(self, regA, regB):
+        return self.registers[regA] < self.registers[regB]
+
+    def test_a_lower_e_than_b(self, regA, regB):
+        return self.registers[regA] <= self.registers[regB]
+
+    def is_primal(self, reg):
+        n = self.registers[reg]
+        start = 2;
+
+        while start <= math.sqrt(n):
+            if n % start < 1:
+                return print("False")
+            start += 1
+
+        return print(n > 1)
+
+    def remainder_division_of_a_by_b(self,regA,regB):
+        return self.registers[regA] % self.registers[regB]
+
+    def factorial(self,reg):
+        return math.factorial(self.registers[reg])
+
+    def potential(self,regA,regB):
+        return self.registers[regA] ** self.registers[regB]
