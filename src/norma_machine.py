bcf1884ed81addbd4c061608b0053f1e6581f5fb
Robson Cruz <ghost0092@gmail.com>
2018-03-27 20:06:23 -0300

    Hotfix-0.4.1 Resolvendo o bug da multiplicação.

--- src/norma_machine.py
+++ src/norma_machine.py
@@ -16,10 +16,12 @@ class NormaMachine:
         msg += "D: ({},{})".format(self.registers["D"]["signal"], self.registers["D"]["magnitude"])
         return msg
 
+    # TODO inplement change of signal as a function
     def change_signal(self, reg):
         pass
 
     def set_0_to_reg(self, reg):
+        print("{}:= 0".format(reg))
         while True:
             if self.registers[reg]["magnitude"] == 0:
                 self.registers[reg]["signal"] = 0
@@ -31,6 +33,7 @@ class NormaMachine:
                 print(self)
 
     def set_n_to_reg(self, reg, n):
+        print("{}:= {}".format(reg, n))
         self.set_0_to_reg(reg)
         cont = abs(n)
         if n < 0: self.registers[reg]["signal"] = 1
@@ -42,7 +45,8 @@ class NormaMachine:
                 cont -= 1
                 print(self)
 
-    def add_b_to_a(self, reg_b, reg_a):
+    def add_b_to_a(self, reg_b="B", reg_a="A"):
+        print("{0}:={0} + {1}".format(reg_a, reg_b))
         while True:
             if self.registers[reg_b]["magnitude"] == 0:
                 break
@@ -79,7 +83,8 @@ class NormaMachine:
                     self.registers[reg_b]["signal"] = 0
                 print(self)
 
-    def add_b_to_a_with_c(self, reg_b, reg_a, reg_c):
+    def add_b_to_a_with_c(self, reg_b="B", reg_a="A", reg_c="C"):
+        print("{0}:={0} + {1} usando {2}".format(reg_a, reg_b, reg_c))
         self.set_0_to_reg(reg_c)
         self.registers[reg_c]["signal"] = self.registers[reg_b]["signal"]
         while True:
@@ -121,24 +126,34 @@ class NormaMachine:
                 print(self)
         self.add_b_to_a(reg_c, reg_b)
 
-    def set_b_to_a_with_c(self, reg_b, reg_a, reg_c):
+    def set_b_to_a_with_c(self, reg_b="B", reg_a="A", reg_c="C"):
+        print("{}:= {} usando {}".format(reg_a, reg_b.reg_c))
         self.set_0_to_reg(reg_a)
         self.add_b_to_a_with_c(reg_b, reg_a, reg_c)
 
-    def mult_a_with_b_with_c_and_d(self,reg_a,reg_b,reg_c,reg_d):
+    def mult_a_with_b_with_c_and_d(self, reg_a="A", reg_b="B", reg_c="C", reg_d="D"):
+        print("{0}:={0} x {1} usando {2}, {3}".format(reg_a, reg_b, reg_c, reg_d))
+        signal = 0
+        if (self.registers[reg_a]["signal"] == 0 and self.registers[reg_b]["signal"] == 0) or \
+                (self.registers[reg_a]["signal"] == 1 and self.registers[reg_b]["signal"] == 1):
+            pass
+        else:
+            signal = 1
         self.set_0_to_reg(reg_c)
-        self.add_b_to_a(reg_a,reg_c)
+        self.add_b_to_a(reg_a, reg_c)
+
         while True:
             if self.registers[reg_c]["magnitude"] == 0:
                 break
             else:
-                self.add_b_to_a_with_c(reg_b,reg_a,reg_d)
-                self.registers[reg_c]["magnitude"] = self.registers[reg_c]["magnitude"] -1
+                self.add_b_to_a_with_c(reg_b, reg_a, reg_d)
+                self.registers[reg_c]["magnitude"] = self.registers[reg_c]["magnitude"] - 1
                 if self.registers[reg_c]["magnitude"] == 0:
                     self.registers[reg_c]["signal"] = 0
                 print(self)
+        self.registers[reg_a]["signal"] = signal
 
-    def test_a_lower_eq_than_b_auxc_auxd(self,reg_a="A",reg_b="B",reg_c="C",reg_d="D"):
+    def test_a_lower_eq_than_b_auxc_auxd(self, reg_a="A", reg_b="B", reg_c="C", reg_d="D"):
         if self.registers[reg_a]["signal"] == 0:
             if self.registers[reg_b]["signal"] == 0:
                 pass
@@ -179,7 +194,7 @@ class NormaMachine:
             self.registers[reg_b]["magnitude"] = self.registers[reg_b]["magnitude"] + 1
             self.registers[reg_d]["magnitude"] = self.registers[reg_d]["magnitude"] - 1
 
-    def test_a_lower_than_b_auxc_auxd(self,reg_a="A",reg_b="B",reg_c="C",reg_d="D"):
+    def test_a_lower_than_b_auxc_auxd(self, reg_a="A", reg_b="B", reg_c="C", reg_d="D"):
         if self.registers[reg_a]["signal"] == 0:
             if self.registers[reg_b]["signal"] == 0:
                 pass
