81557578f4b050e9c389b98155138db03b5a11b1
Robson Cruz <ghost0092@gmail.com>
2018-03-27 11:13:23 -0300

    Some code cleanup. Started adding docstring and code documentation

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 from src.norma_machine import NormaMachine
 
-nm = NormaMachine()
 
-nm.set_n_to_reg("A",1)
-nm.is_primal("A")
-nm.set_n_to_reg("A",5)
-nm.is_primal("A")
-nm.set_n_to_reg("A",-1)
-nm.set_n_to_reg("A",-4)
-nm.set_n_to_reg("A",-2)
-nm.set_n_to_reg("A",4)
-nm.set_n_to_reg("A",9)
-nm.is_primal("A")
-nm.set_n_to_reg("A",11)
-nm.is_primal("A")
+def main():
+    nm = NormaMachine() # instantiate the machine
+
+    nm.set_n_to_reg("A", 1)
+    print(nm.is_primal("A"))
+    nm.set_n_to_reg("A", 5)
+    print(nm.is_primal("A"))
+    nm.set_n_to_reg("A", -1)
+    nm.set_n_to_reg("A", -4)
+    nm.set_n_to_reg("A", -2)
+    nm.set_n_to_reg("A", 4)
+    nm.set_n_to_reg("A", 9)
+    print(nm.is_primal("A"))
+    nm.set_n_to_reg("A", 11)
+    print(nm.is_primal("A"))
 
 
+# Central point for all the program startup
+if __name__ == '__main__':
+    main()
--- src/norma_machine.py
+++ src/norma_machine.py
@@ -4,6 +4,11 @@ import math
 class NormaMachine:
 
     def __init__(self):
+        """
+        Norma Machine constructor.
+
+        Starts all registers at zero
+        """
         self.registers = {
             "A": 0,
             "B": 0,
@@ -12,6 +17,11 @@ class NormaMachine:
         }
 
     def __str__(self):
+        """
+        Print function
+        :return: msg with register info
+        :rtype: str
+        """
         msg = ""
         msg += "A:({},{}) | ".format(0 if self.registers["A"] >= 0 else 1, abs(self.registers["A"]))
         msg += "B:({},{}) | ".format(0 if self.registers["B"] >= 0 else 1, abs(self.registers["B"]))
@@ -20,6 +30,15 @@ class NormaMachine:
         return msg
 
     def set_0_to_reg(self, register):
+        """
+        Set 0 to a passed register
+
+        register := 0
+
+        :param register: target register
+        :type register: str
+        :return: None
+        """
         print("Set 0 to {}".format(register))
         print(self)
         while self.registers[register] != 0:
@@ -30,6 +49,17 @@ class NormaMachine:
             print(self)
 
     def set_n_to_reg(self, reg, n):
+        """
+        Set N to a passed register
+
+        reg := n
+
+        :param reg: target register
+        :type reg: str
+        :param n: new value for the register
+        :type n: int
+        :return: None
+        """
         print("Set {} to {}".format(n, reg))
         while self.registers[reg] != n:
             if n == 0:
@@ -40,36 +70,47 @@ class NormaMachine:
                 self.registers[reg] -= 1
             print(self)
 
-    def add_b_to_a(self, regB, regA):
+    def add_b_to_a(self, reg_b, reg_a):
+        """
+        Add value of reg B to A without conserving value
+
+        A:= A+B
+
+        :param reg_b: a given register (will be 0 after operation)
+        :type reg_b: str
+        :param reg_a: target register
+        :type reg_a: str
+        :return: None
+        """
         print("Add B to A")
-        while self.registers[regB] != 0:
-            if self.registers[regB] > 0:
-                self.registers[regA] += 1
-                self.registers[regB] -= 1
+        while self.registers[reg_b] != 0:
+            if self.registers[reg_b] > 0:
+                self.registers[reg_a] += 1
+                self.registers[reg_b] -= 1
             else:
-                self.registers[regA] -= 1
-                self.registers[regB] += 1
+                self.registers[reg_a] -= 1
+                self.registers[reg_b] += 1
             print(self)
 
-    def add_b_to_a_with_c(self, regB, regA, regC):
+    def add_b_to_a_with_c(self, reg_b, reg_a, reg_c):
         print("Add B to A with C")
-        while self.registers[regB] != 0:
-            if self.registers[regB] > 0:
-                self.registers[regA] += 1
-                self.registers[regB] -= 1
-                self.registers[regC] += 1
+        while self.registers[reg_b] != 0:
+            if self.registers[reg_b] > 0:
+                self.registers[reg_a] += 1
+                self.registers[reg_b] -= 1
+                self.registers[reg_c] += 1
             else:
-                self.registers[regA] -= 1
-                self.registers[regB] += 1
-                self.registers[regC] -= 1
+                self.registers[reg_a] -= 1
+                self.registers[reg_b] += 1
+                self.registers[reg_c] -= 1
             print(self)
-        while self.registers[regC] != 0:
-            if self.registers[regC] > 0:
-                self.registers[regB] += 1
-                self.registers[regC] -= 1
+        while self.registers[reg_c] != 0:
+            if self.registers[reg_c] > 0:
+                self.registers[reg_b] += 1
+                self.registers[reg_c] -= 1
             else:
-                self.registers[regB] -= 1
-                self.registers[regC] += 1
+                self.registers[reg_b] -= 1
+                self.registers[reg_c] += 1
             print(self)
 
     def set_b_to_a_with_c(self):
@@ -84,28 +125,28 @@ class NormaMachine:
                 self.add_b_to_a_with_c("B", "A", "D")
                 self.registers["C"] -= 1
 
-    def test_a_lower_than_b(self, regA, regB):
-        return self.registers[regA] < self.registers[regB]
+    def test_a_lower_than_b(self, reg_a, reg_b):
+        return self.registers[reg_a] < self.registers[reg_b]
 
-    def test_a_lower_e_than_b(self, regA, regB):
-        return self.registers[regA] <= self.registers[regB]
+    def test_a_lower_e_than_b(self, reg_a, reg_b):
+        return self.registers[reg_a] <= self.registers[reg_b]
 
     def is_primal(self, reg):
         n = self.registers[reg]
-        start = 2;
+        start = 2
 
         while start <= math.sqrt(n):
             if n % start < 1:
-                return print("False")
+                return False
             start += 1
 
-        return print(n > 1)
+        return n > 1
 
-    def remainder_division_of_a_by_b(self,regA,regB):
-        return self.registers[regA] % self.registers[regB]
+    def remainder_division_of_a_by_b(self, reg_a, reg_b):
+        return self.registers[reg_a] % self.registers[reg_b]
 
-    def factorial(self,reg):
+    def factorial(self, reg):
         return math.factorial(self.registers[reg])
 
-    def potential(self,regA,regB):
-        return self.registers[regA] ** self.registers[regB]
+    def potential(self, reg_a, reg_b):
+        return self.registers[reg_a] ** self.registers[reg_b]
