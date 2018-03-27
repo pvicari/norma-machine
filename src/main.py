905e2bbf0b269b31c97be03100a93e25161ee06d
phpvicari <peehvicari@gmail.com>
2018-03-27 17:14:03 -0300

    mandando tudo zuado, multiplicacao com sinal errado e lower nao fazendo para negativo menor que negativo

--- src/main.py
+++ src/main.py
@@ -5,23 +5,11 @@ from src.norma_machine import NormaMachine
 def main():
     nm = NormaMachine()  # instantiate the machine
 
-    nm.set_n_to_reg("A", 1)
-    print(nm.is_primal("A"))
-    nm.set_n_to_reg("A", 5)
-    print(nm.is_primal("A"))
-    nm.set_n_to_reg("A", -1)
-    nm.set_n_to_reg("A", -4)
-    nm.set_n_to_reg("A", -2)
-    nm.set_n_to_reg("A", 4)
-    nm.set_n_to_reg("A", 9)
-    print(nm.is_primal("A"))
-    nm.set_n_to_reg("A", 11)
-    nm.set_n_to_reg("B", 4)
-    print(nm.is_primal("A"))
-    print(nm.potential("B", "A"))
-    print(nm.factorial("B"))
-    nm.set_b_to_a_with_c()
+    nm.set_n_to_reg("A", -3)
+    nm.set_n_to_reg("B", -2)
+    nm.test_a_lower_than_b_auxc_auxd()
 
+    #verificar o pq da falso
 
 # Central point for all the program startup
 if __name__ == '__main__':
