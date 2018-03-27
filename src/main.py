d74a6a35cc9c9adab461eba77c83527203799900
phpvicari <peehvicari@gmail.com>
2018-03-26 21:19:39 -0300

    fazendo do jeito certo

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from src.norma_machine import NormaMachine
 
 nm = NormaMachine()
 
-nm.set_n_to_reg("A",8)
 nm.set_n_to_reg("A",1)
-nm.set_n_to_reg("A",2)
-nm.set_n_to_reg("A",-7)
+nm.set_n_to_reg("A",5)
+nm.set_n_to_reg("A",-1)
+nm.set_n_to_reg("A",-4)
 nm.set_n_to_reg("A",-2)
+nm.set_n_to_reg("A",4)
+nm.set_n_to_reg("A",2)
+
--- src/norma_machine.py
+++ src/norma_machine.py
@@ -1,68 +1,70 @@
 class NormaMachine:
 
     def __init__(self):
-        self.registers = dict(
-            A={
-                "signal": 0,
-                "magnitude": 0,
-            },
-            B={
-                "signal": 0,
-                "magnitude": 0,
-            },
-            C={
-                "signal": 0,
-                "magnitude": 0,
-            },
-            D={
-                "signal": 0,
-                "magnitude": 0,
-            },
-        )
+        self.registers = {
+            "A": 0,
+            "B": 0,
+            "C": 0,
+            "D": 0,
+        }
 
     def __str__(self):
         msg = ""
-        msg += "A: ({},{}) | ".format(self.registers["A"]["signal"], self.registers["A"]["magnitude"])
-        msg += "B: ({},{}) | ".format(self.registers["B"]["signal"], self.registers["B"]["magnitude"])
-        msg += "C: ({},{}) | ".format(self.registers["C"]["signal"], self.registers["C"]["magnitude"])
-        msg += "D: ({},{})".format(self.registers["D"]["signal"], self.registers["D"]["magnitude"])
+        msg += "A:({},{}) | ".format(0 if self.registers["A"] >= 0 else 1, abs(self.registers["A"]))
+        msg += "B:({},{}) | ".format(0 if self.registers["B"] >= 0 else 1, abs(self.registers["B"]))
+        msg += "C:({},{}) | ".format(0 if self.registers["C"] >= 0 else 1, abs(self.registers["C"]))
+        msg += "D:({},{})".format(0 if self.registers["D"] >= 0 else 1, abs(self.registers["D"]))
         return msg
 
-    def set_0_to_reg(self, reg):
-        print("Set 0 to {}".format(reg))
+    def set_0_to_reg(self, register):
+        print("Set 0 to {}".format(register))
         print(self)
-        while self.registers[reg]["magnitude"] != 0:
-            self.registers[reg]["magnitude"] -= 1
+        while self.registers[register] != 0:
+            if self.registers[register] > 0:
+                self.registers[register] -= 1
+            else:
+                self.registers[register] += 1
             print(self)
-        self.registers[reg]["signal"] = 0
-        print(self)
 
     def set_n_to_reg(self, reg, n):
         print("Set {} to {}".format(n, reg))
-        print(self)
-        register = self.registers[reg]
-        number = abs(n)
-        while register["magnitude"] != number:
-            if (n == 0):
+        while self.registers[reg] != n:
+            if n == 0:
                 self.set_0_to_reg(reg)
-            elif (n > 0):
-                if register["signal"] != 0:  # signal negativo
-                    self.set_0_to_reg(reg)
-                else:
-                    if number > register["magnitude"]:
-                        register["magnitude"] += 1
-                    else:
-                        register["magnitude"] -= 1
+            elif n > self.registers[reg]:
+                self.registers[reg] += 1
             else:
-                if register["signal"] == 0:  # signal positivo
-                    self.set_0_to_reg(reg)
-                    register["signal"] = 1
-                else:
-                    if number > register["magnitude"]:
-                        register["magnitude"] += 1
-                    else:
-                        register["magnitude"] -= 1
-            if (register["signal"] == 1 and register["magnitude"] == 0):
-                continue
+                self.registers[reg] -= 1
+            print(self)
+
+    def add_b_to_a(self):
+        print("Add B to A")
+        while self.registers["B"] != 0:
+            if self.registers["B"] > 0:
+                self.registers["A"] += 1
+                self.registers["B"] -= 1
             else:
-                print(self)
+                self.registers["A"] -= 1
+                self.registers["B"] += 1
+            print(self)
+
+    def add_b_to_a_with_c(self):
+        print("Add B to A with C")
+        while self.registers["B"] != 0:
+            if self.registers["B"] > 0:
+                self.registers["A"] += 1
+                self.registers["B"] -= 1
+                self.registers["C"] += 1
+            else:
+                self.registers["A"] -= 1
+                self.registers["B"] += 1
+                self.registers["C"] -= 1
+            print(self)
+        while self.registers["C"] != 0:
+            if self.registers["C"] > 0:
+                self.registers["B"] += 1
+                self.registers["C"] -= 1
+            else:
+                self.registers["B"] -= 1
+                self.registers["C"] += 1
+            print(self)
