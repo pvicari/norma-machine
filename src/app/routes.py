e68a15f2ce653e47bdc8d57a73ed8da5f2132a4f
deadpyxel <ghost0092@gmail.com>
2018-03-31 11:55:17 -0300

    Now showing all the registers, stack, and stack pointer.

--- src/app/routes.py
+++ src/app/routes.py
@@ -19,13 +19,28 @@ def set_0_to_reg():
 
     nm.set_0_to_reg(request.form['reg'])
 
+    return jsonify({'registers': nm.registers,
+                    'stack': nm.stack,
+                    'stack_pointer': nm.stack_pointer})
+
+
+@app.route('/set-n-to-reg', methods=['POST'])
+def set_n_to_reg():
+    assert request.path == '/set-n-to-reg'
+    assert request.method == 'POST'
+
+    nm.set_n_to_reg(request.form['reg'], request.form['val'])
+
     return jsonify({'val': nm.get_reg_magnitude("A")})
 
 
-@app.route('/set-n-to-reg', methods=['POST'])
-@app.route('/set-n-to-reg', methods=['POST'])
-@app.route('/set-n-to-reg', methods=['POST'])
-@app.route('/set-n-to-reg', methods=['POST'])
-@app.route('/set-n-to-reg', methods=['POST'])
-def set_n_to_reg():
+@app.route('/add-b-to-a', methods=['POST'])
+@app.route('/add-b-to-a-with-c', methods=['POST'])
+@app.route('/set-b-to-a-with-c', methods=['POST'])
+@app.route('/mult-a-with-b', methods=['POST'])
+@app.route('/test-a-lower-eq-than-b', methods=['POST'])
+@app.route('/test-a-lower-than-b', methods=['POST'])
+@app.route('/factorial', methods=['POST'])
+@app.route('/power', methods=['POST'])
+def todo():
     pass
